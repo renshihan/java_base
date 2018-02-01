150d06e213b7167db5145a4ad047dad1dde9eb7c
renshihan <379595488@qq.com>
2018-02-01 17:25:42 +0800

    2次更新

---
+++ archaius-study/src/main/java/com/renshihan/study/archaius/study/ArchaiusStudyApplication.java
@@ -0,0 +1,12 @@
+package com.renshihan.study.archaius.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchaiusStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchaiusStudyApplication.class, args);
+	}
+}
