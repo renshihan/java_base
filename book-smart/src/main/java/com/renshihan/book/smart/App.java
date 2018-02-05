7a7f78241f222bdd2ecf8560e940b31b86b03cb6
任时汉 <379595488@qq.com>
2018-02-05 22:07:10 +0800

    《架构探险 1》

---
+++ book-smart/src/main/java/com/renshihan/book/smart/App.java
@@ -0,0 +1,13 @@
+package com.renshihan.book.smart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
