6e623d43953f5683bb88cab3fd6ffb565cf2f4bc
renshihan <379595488@qq.com>
2018-03-21 16:13:12 +0800

    新加入netty-server和netty-client

---
+++ book-netty-client/src/main/java/com/renshihan/study/booknetty/BookNettyApplication.java
@@ -0,0 +1,12 @@
+package com.renshihan.study.booknetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNettyApplication.class, args);
+	}
+}
