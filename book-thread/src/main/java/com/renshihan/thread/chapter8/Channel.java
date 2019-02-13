d6c590535d4fc0bc5d733e3ed059430942c9aa9d
任时汉 <ren_sh@suixingpay.com>
2019-02-13 14:17:30 +0800

    目录修改

---
+++ book-thread/src/main/java/com/renshihan/thread/chapter8/Channel.java
@@ -0,0 +1,15 @@
+/**
+ * All rights Reserved, Designed By Suixingpay.
+ *
+ * @author: ren_sh[ren_sh@suixingpay.com]
+ * @date: 2019/2/13 14:07
+ * @Copyright ©2019 Suixingpay. All rights reserved.
+ * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
+ */
+package com.renshihan.thread.chapter8;
+
+/**
+ *
+ */
+public class Channel {
+}
